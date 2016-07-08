a9d6d1cddd8c1f152854d819142750d35a131ea8
Saurabh Nanda <saurabh@vacationlabs.com>
2016-07-08 16:20:12 +0530

    * Got the scaffolded yesod app to run.

--- app/main.hs
+++ app/main.hs
@@ -3,3 +3,44 @@ import Application (appMain)
 
 main :: IO ()
 main = appMain
+
+
+-- {-# LANGUAGE OverloadedStrings          #-}
+-- import Nightwatch.Types
+-- import Nightwatch.DBTypes
+-- import Nightwatch.Telegram
+-- import Nightwatch.Webapp
+-- -- import Nightwatch.Websocket
+-- -- import Nightwatch.Aria2
+-- -- import Control.Concurrent.Async
+-- import Control.Concurrent.Chan
+-- -- import Database.Persist.Sql
+-- import Database.Persist.Sqlite
+-- -- import Control.Monad.Trans.Resource (runResourceT)
+-- import Control.Monad.Logger (runStderrLoggingT)
+-- import Control.Monad.IO.Class  (liftIO)
+-- import System.Environment(getEnv)
+-- import Control.Lens
+-- import qualified Data.Text as T
+
+-- onMessage :: Aria2Gid -> IO ()
+-- onMessage gid = putStrLn $ "Received response " ++ (show gid)
+
+
+-- main :: IO ()
+-- main = runStderrLoggingT $ withSqlitePool "nightwatch.db" 5 $ \pool -> liftIO $ do
+--   [cId, cSecret, botToken] <- sequence [getEnv "GOOGLE_CLIENT_ID", getEnv "GOOGLE_CLIENT_SECRET", getEnv "TELEGRAM_TOKEN"]
+--   tgOutChan <- newChan
+--   let nwConfig = (def :: NwConfig)
+--         & googleClientId .~ (T.pack cId)
+--         & googleClientSecret .~ (T.pack cSecret)
+--         & tgramBotToken .~ botToken
+--         & dbPool .~ pool
+--         & tgramOutgoingChannel .~ tgOutChan
+--         & aria2Command .~ "/Users/saurabhnanda/projects/nightwatch/aria2-1.19.3/bin/aria2c"
+--         & aria2DownloadDir .~ "/Users/saurabhnanda/projects/nightwatch/downloads"
+--   -- runMigrations pool
+--   startAria2 nwConfig
+--   startTelegramBot nwConfig
+--   startWebapp nwConfig
+
